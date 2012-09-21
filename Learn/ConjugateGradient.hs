1052ab974728c76965ceeb22cb21bb34e0e80959
Paul Graphov <graphov@gmail.com>
2012-09-21 13:41:20 +0400

    small step...

--- Learn/ConjugateGradient.hs
+++ Learn/ConjugateGradient.hs
@@ -16,17 +16,24 @@ aMax = 1.0
 interpolateNormMin x1
 
 -- Cubic Hermite spline based on values of function and its first derivatives
-interpolateMin :: Double -> Double -> Double -> Double -> Double
-interpolateMin p0 p1 m0 m1 = case cubicMin a b c of
-  Nothing -> minBound
-  Just x -> if spline x < minBoundVal then x else minBound
+-- If there is no extremum in the interval or if it is not minimum, just bisect
+interpolateMinNorm :: Double -> Double -> Double -> Double -> Maybe Double
+interpolateMinNorm p0 p1 m0 m1 = do
+  cm <- cubicMin a b c
+  let (minBound, minBoundVal) = if p0 < p1 then (0, p0) else (1, p1)
+      a = 2*p0 + m0 -2*p1 + m1
+      b = -3*p0 - 2*m0 + 3*p1 - m1
+      c = m0
+      d = p1
+      spline x = x*x*x*a + x*x*b + x*c + d
+  if spline x < minBoundVal then return x else Nothing
+
+-- normalize values as if we have values of function and tangent at 0 and 1, then find minimum
+cubicOrBisect :: Double -> Double -> Double -> Double -> Double -> Double -> Double
+cubicOrBisect a0 a1 p0 p1 m0 m1 = a0 + x * len
   where
-    (minBound, minBoundVal) = if p0 < p1 then (0, p0) else (1, p1)
-    a = 2*p0 + m0 -2*p1 + m1
-    b = -3*p0 - 2*m0 + 3*p1 - m1
-    c = m0
-    d = p1
-    spline x = x*x*x*a + x*x*b + x*c + d -- we omit the last coefficient as it doesn't affect comparison
+    len = a1 - a0
+    x = fromMaybe 0.5 $ interpolateMinNorm p0 p1 (m0 * len) (m1 * len)
 
 -- find minimum of cubic polynomial (last coefficient is omitted)
 cubicMin :: Double -> Double -> Double -> Maybe Double
@@ -44,7 +51,7 @@ lineSearch sc c1 c2 fn start dir =
      step 0 (chose 0 aMax) p0  p0' False
   where
     phi a = do -- phi(a) = fn(start + a * dir), univariate representation of step length selection problem
-      x <- computeP $ start +^ R.map(* a) dir
+      x <- computeP $ start +^ R.map(* a) di
       (val, grad) <- fn x
       grad' <- foldAllP <$> grad *^ dir -- projection of gradient on search direction
       return (val, grad')
@@ -56,10 +63,7 @@ lineSearch sc c1 c2 fn start dir =
                  | pa2' > 0 -> zoom a2 a1
                  | otherwise -> step a2 (chose a2 aMax) pa2 pa2' False
 
-    zoom a1 a2 pa1 pa2 = 
-                   
-
-  
+    zoom a1 a2 pa1 pa2 pa1' pa2' =
 
 conjugateGradient :: Monad m => StopCondition -> Function -> UVec -> m UVec
 conjugateGradient = ...
