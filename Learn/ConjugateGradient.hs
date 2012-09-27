167a9128270b05d57c0bc64489d5b99ec10f6eda
Paul Graphov <graphov@gmail.com>
2012-09-27 16:13:23 +0400

    Debug in progress

--- Learn/ConjugateGradient.hs
+++ Learn/ConjugateGradient.hs
@@ -2,13 +2,20 @@
 
 module Learn.ConjugateGradient where
 
+import Debug.Trace
 import Control.Monad
 
 import Data.Maybe
-import Data.Array.Repa as R
+import Data.Array.Repa as R hiding ((++))
 
 import Learn.Types
 import Learn.Optimization
+
+dbg :: Show a => a -> a
+dbg a = traceShow a a
+
+dbgm :: Show a => String -> a -> a
+dbgm m a = trace (m ++ ": " ++ show a) a
 
 -- Underscores like in x_ means roughly "x value from previous iteration" whenever new x is calculated
 
@@ -18,11 +25,11 @@ import Learn.Optimization
 
 -- ^ procedure to chose next step length. TODO: tune it
 chose :: Double -> Double -> Double
-chose low high = low + (high - low) / 10
+chose low high = low + (high - low) / 100
 
 -- ^ maximum step length, TODO: tune it
 aMax :: Double
-aMax = 1.0
+aMax = 1000.0
 
 -- ^ constants for strong Wolfe conditions recommended by Nocedal:
 c₁ :: Double
@@ -30,7 +37,7 @@ c₁ = 0.0001
 c₂ :: Double
 c₂ = 0.1
 
--- Cubic Hermite spLowine based on values of function and its first derivatives
+-- Cubic Hermite spline based on values of function and its first derivatives
 -- If there is no extremum in the interval or if it is not minimum, just bisect
 -- Also if it is too close to the boundary, it is also rejected to prevent slow convergence.
 interpolateMinNorm :: Double -> Double -> Double -> Double -> Maybe Double
@@ -52,7 +59,9 @@ cubicOrBisect :: Double -> Double -> Double -> Double -> Double -> Double -> Dou
 cubicOrBisect a₀ a₁ p₀ p₁ m₀ m₁ = a₀ + x * len
   where
     len = a₁ - a₀
-    x = fromMaybe 0.5 $ interpolateMinNorm p₀ p₁ (m₀ * len) (m₁ * len)
+    x = fromMaybe 0.5 $ if a₀ < a₁
+                        then interpolateMinNorm p₀ p₁ (m₀ * len) (m₁ * len)
+                        else interpolateMinNorm p₀ p₁ (m₀ * len) (m₁ * len)
 
 -- find minimum of cubic polynomial (last coefficient is omitted)
 -- to avoid endless loops somewhere in the calling code (zoom)
@@ -63,45 +72,78 @@ cubicMin a b c = if det > 0
   where
     det = b*b-3*a*c
 
+-- ^ Strong Wolfe conditions check, for testing line search algorithms
+strongWolfe :: Double -> Double -> Double -> UVec -> UVec -> UVec -> Bool
+strongWolfe a f₀ f δf₀ δf p =
+  f <= f₀ + c₁ * a * sumAllS (δf₀ *^ p) &&            -- sufficient decrease
+  abs (sumAllS (δf *^ p)) <= abs (c₂ * sumAllS (δf₀ *^ p)) -- curvature
+
+
+{-
+FIXME: fix cubic interpolation for interval with swapped bounds.
+FIXME: add some checks if we our moves approach machine precision limit. And do something about it.
+-}
+
 -- ^ line search algorithm form the book:
--- Jorge Nocedal, StepHighen J. Wright, Numerical Optimization, Second Edition, Algorithm 3.5
+-- Jorge Nocedal, Stephen J. Wright, Numerical Optimization, Second Edition, Algorithm 3.5
 lineSearch :: Monad m => Function m -> UVec -> UVec -> m (UVec, Double, UVec)
 lineSearch fn start dir =
   do
-    (_, p₀, p₀', _) <- φ 0 -- TODO: value and derivative at starting point should already be calculated
-    
-    let step a₁ a₂ p₁ p₁' first = do
-          (x₂, p₂, p₂', gp₂) <- φ a₂
+    (_, f₀, f₀', δf₀) <- φ 0 -- TODO: value and derivative at starting point should already be calculated
+    let step a₁ a₂ f₁ f₁' first = do
+          (x₂, f₂, f₂', δf₂) <- φ a₂
           case () of 
-            _ | p₂ > p₀ + c₁ * a₂ * p₀' || (p₂ > p₁ && not first) -> zoom a₁ a₂ p₁ p₂ p₁' p₂'
-              | abs p₂' <= -c₂ * p₀' -> return (x₂, p₂, gp₂)
-              | p₂' > 0 -> zoom a₂ a₁ p₂ p₁ p₂' p₁'
-              | otherwise -> step a₂ (chose a₂ aMax) p₂ p₂' False
+            _ | f₂ > f₀ + c₁ * a₂ * f₀' || (f₂ > f₁ && not first) -> trace "zoom1" $ zoom a₁ a₂ f₁ f₂ f₁' f₂'
+              | abs f₂' <= -c₂ * f₀' -> trace "gotcha in step" $ return (a₂, x₂, f₂, δf₂)
+              | f₂' > 0 -> trace "zoom2" $ zoom a₂ a₁ f₂ f₁ f₂' f₁'
+              | otherwise -> trace "recur" $ step a₂ (chose a₂ aMax) f₂ f₂' True
 
-        zoom aLow aHigh pLow pHigh pLow' pHigh' = do
-          let aⱼ = cubicOrBisect aⱼ aHigh pLow pHigh pLow' pHigh'
-          (xⱼ, pⱼ, pⱼ', gpⱼ) <- φ aⱼ
-          case () of
-            _ | pⱼ > p₀ + c₁ * aLow * p₀' || pⱼ >= pLow -> zoom aLow aⱼ pLow pⱼ pLow' pⱼ' -- move upper bound
-              | abs pⱼ' <= - c₂ * p₀' -> return (xⱼ, pⱼ, gpⱼ)
-              | pⱼ' * (aHigh - aLow) >= 0 -> zoom aLow aⱼ pLow pⱼ pLow' pⱼ' -- move upper bound
-              | otherwise -> zoom aⱼ aHigh pⱼ pHigh pⱼ' pHigh' -- move lower bound
+        zoom aLow aHigh fLow fHigh fLow' fHigh' = do
+          --let aⱼ = cubicOrBisect aⱼ aHigh fLow fHigh fLow' fHigh'
+          let aⱼ = dbg $ (aLow + aHigh) / 2
+          (xⱼ, fⱼ, fⱼ', δfⱼ) <- φ aⱼ
+          traceShow (aLow, fLow, fLow', aⱼ, fⱼ, fⱼ', aHigh, fHigh, fHigh') $
+            case () of
+              _ | fⱼ > f₀ + c₁ * aLow * f₀' || fⱼ >= fLow -> zoom aLow aⱼ fLow fⱼ fLow' fⱼ' -- move uffer bound
+                | abs fⱼ' <= - c₂ * f₀' -> return (aⱼ, xⱼ, fⱼ, δfⱼ)
+                | fⱼ' * (aHigh - aLow) >= 0 -> zoom aLow aⱼ fLow fⱼ fLow' fⱼ' -- move uffer bound
+                | otherwise -> zoom aⱼ aHigh fⱼ fHigh fⱼ' fHigh' -- move lower bound
 
-    step 0 (chose 0 aMax) p₀  p₀' False
+    (a, x, f, δf) <- trace ("calling step with f₀ = " ++ show f₀ ++ " and f₀' = " ++ show f₀') $ step 0 (chose 0 aMax) f₀  f₀' False
+    let wolfe1 = f <= f₀ + c₁ * a * sumAllS (δf₀ *^ dir)
+        wolfe2 = abs (sumAllS (δf *^ dir)) <= -c₂ * sumAllS (δf₀ *^ dir)
+    trace ("line search strong wolfe conditions checks: " ++ show wolfe1 ++ " " ++ show wolfe2) $ return (x, f, δf)
+
 
   where
     φ a = do -- φ(a) = fn(start + a * dir), univariate representation of step length selection problem
-      x <- computeP $ start +^ R.map (* a) dir
+      let x = computeS $ start +^ R.map (* a) dir
+      (p, δp) <- fn x
+      let p' = sumAllS $ δp *^ dir -- projection of gradient on search direction
+      return (x, p, p', δp)
+
+{-
+
+dumb plug:
+
+lineSearch :: Monad m => Function m -> UVec -> UVec -> m (UVec, Double, UVec)
+lineSearch fn start dir = do
+  (x, val, _, grad) <- φ 1
+  return (x, val, grad)
+  where
+    φ a = do -- φ(a) = fn(start + a * dir), univariate representation of step length selection problem
+      let x = computeS $ start +^ R.map (* a) dir
       (p, gp) <- fn x
-      p' <- sumAllP $ gp *^ dir -- projection of gradient on search direction
+      let p' = sumAllS $ gp *^ dir -- projection of gradient on search direction
       return (x, p, p', gp)
+-}
 
 -- Polack-Ribiere conjugate gradient method 
 conjugateGradient :: Monad m => StopCondition -> Function m -> UVec -> m (UVec, Double)
 conjugateGradient sc fn start = 
   do
     (_, f₀') <- fn start  -- get value and gradient at start
-    p₀ <- computeP $ R.map negate f₀' -- initial search direction is the steepest descent direction
+    let p₀ = computeS $ R.map negate f₀' -- initial search direction is the steepest descent direction
     loop start p₀ f₀' 0 0
 
   where
@@ -109,13 +151,13 @@ conjugateGradient sc fn start =
 
     loop x_ p_ f_' i ir = do
       (x, f, f') <- lineSearch fn x_ p_
-      beta <- betaPR f_' f'
-      let (betaPlus, restart) = if beta > 0 || ir >= n -- if beta went below zero or at least every n'th iteration
-                                then (beta, False)    -- we restart using steepest descent direction
-                                else (0, True)
-      p <- computeP $ R.map (* betaPlus) p_ -^ f'
+      let beta = betaPR f_' f'
+          (betaPlus, restart) = trace ("beta is: " ++ show beta) $ if beta > 0 && ir < n -- if beta went below zero or at least every n'th iteration
+                                                                   then (beta, False)    -- we restart using steepest descent direction
+                                                                   else (0, True)
+      let p = computeS $ R.map (* betaPlus) p_ -^ f'
       if checkIter sc i -- TODO: add tolerance test
         then return (x, f)
         else loop x p f' (i+1) (if restart then 0 else ir+1)
 
-    betaPR prev cur = (/) `liftM` sumAllP (prev *^ prev) `ap` sumAllP (cur *^ (cur -^ prev))
+    betaPR prev cur = sumAllS (cur *^ cur) / sumAllS (prev *^ (prev{- -^ prev-}))
